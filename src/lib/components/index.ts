eb8a8128046c42960f37db23c0d15edfd54e2d9a
mukut <mukutb.mac@gmail.com>
2020-03-03 17:57:12 +0530

    routes fixes

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,5 +1,5 @@
 export { default as AllTemplates } from './AllTemplates';
 export * from './AllTemplates';
 
-export { default as EditTemplate } from './EditTemplate';
-export * from './EditTemplate';
+export { default as EditTemplate } from './AddEditTemplate';
+export * from './AddEditTemplate';
